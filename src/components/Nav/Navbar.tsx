30d8abbfa6007b0c639127ced420a1d5be087905
olayinka fagbemi <fagbemio123@gmail.com>
2023-07-05 23:50:31 +0100

    styles

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,19 +1,25 @@
-import React from "react";
-import styles from "./Nav.module.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp, FaBars, FaTimes } from "react-icons/fa";
+import styles from "./Nav.module.css";
 
 const Navbar: React.FC = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const scrollToSection = (sectionId: string) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+      section.scrollIntoView({ behavior: "smooth" });
+      setMenuOpen(false);
     }
   };
 
@@ -24,45 +30,82 @@ const Navbar: React.FC = () => {
           <img src="./assets/logo.png" alt="logo" />
         </Link>
       </div>
-      <ul className={styles.nav_menu}>
+      <div className={styles.mobile_menu}>
+        {!menuOpen && (
+          <FaBars className={styles.menu_icon} onClick={toggleMenu} />
+        )}
+        {menuOpen && (
+          <FaTimes className={styles.menu_icon} onClick={toggleMenu} />
+        )}
+      </div>
+      <ul className={`${styles.nav_menu} ${menuOpen ? styles.open : ""}`}>
         <li className={styles.nav_item}>
           <a href="#url">My URLs</a>
         </li>
         <li className={styles.nav_item}>
-          
           <div className={styles.dropdown}>
-            <button className={styles.nav_feature_button} onClick={toggleDropdown}>
-              Features {dropdownOpen ? <FaChevronUp  className={styles.chevron}/>
-               : <FaChevronDown className={styles.chevron}/>}
+            <button
+              className={styles.nav_feature_button}
+              onClick={toggleDropdown}
+            >
+              Features{" "}
+              {dropdownOpen ? (
+                <FaChevronUp className={styles.chevron} />
+              ) : (
+                <FaChevronDown className={styles.chevron} />
+              )}
             </button>
             {dropdownOpen && (
               <ul className={styles.dropdown_menu}>
                 <li className={styles.dropdown_item}>
-                <Link to="#link_shortening" onClick={() => scrollToSection('link_shortening')}>URL Shortening</Link>
-                  
+                  <Link
+                    to="#link_shortening"
+                    onClick={() => scrollToSection("link_shortening")}
+                  >
+                    URL Shortening
+                  </Link>
                 </li>
                 <li className={styles.dropdown_item}>
-                  <Link to="#custom_codes" onClick={() => scrollToSection('custom_codes')}>Custom URLs</Link>
+                  <Link
+                    to="#custom_codes"
+                    onClick={() => scrollToSection("custom_codes")}
+                  >
+                    Custom URLs
+                  </Link>
                 </li>
                 <li className={styles.dropdown_item}>
-                <Link to="#qr_codes" onClick={() => scrollToSection('qr_codes')}>QR Codes</Link>
+                  <Link
+                    to="#qr_codes"
+                    onClick={() => scrollToSection("qr_codes")}
+                  >
+                    QR Codes
+                  </Link>
                 </li>
                 <li className={styles.dropdown_item}>
-                <Link to="#data_analytics" onClick={() => scrollToSection('data_analytics')}>Data Analytics</Link>
+                  <Link
+                    to="#data_analytics"
+                    onClick={() => scrollToSection("data_analytics")}
+                  >
+                    Data Analytics
+                  </Link>
                 </li>
               </ul>
-              )}
-              </div>
+            )}
+          </div>
         </li>
 
         <li className={styles.nav_item}>
-          <a href='#pricing' onClick={() => scrollToSection('pricing')}>Pricing</a>
+          <a href="#pricing" onClick={() => scrollToSection("pricing")}>
+            Pricing
+          </a>
         </li>
         <li className={styles.nav_item}>
           <Link to="#analytics">Analytics</Link>
         </li>
         <li className={styles.nav_item}>
-          <a href="#faqs" onClick={() => scrollToSection('faqs')}>FAQs</a>
+          <a href="#faqs" onClick={() => scrollToSection("faqs")}>
+            FAQs
+          </a>
         </li>
 
         <div className={styles.right_navbar}>
@@ -79,3 +122,89 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
+
+
+
+// import React from "react";
+// import styles from "./Nav.module.css";
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+// const Navbar: React.FC = () => {
+//   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+//   const toggleDropdown = () => {
+//     setDropdownOpen(!dropdownOpen);
+//   };
+//   const scrollToSection = (sectionId: string) => {
+//     const section = document.getElementById(sectionId);
+//     if (section) {
+//       section.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   };
+
+  
+//   return (
+//     <header className={styles.navbar}>
+//       <div className={styles.navbar_logo}>
+//         <Link to="/">
+//           <img src="./assets/logo.png" alt="logo" />
+//         </Link>
+//       </div>
+//       <ul className={styles.nav_menu}>
+//         <li className={styles.nav_item}>
+//           <a href="#url">My URLs</a>
+//         </li>
+//         <li className={styles.nav_item}>
+          
+//           <div className={styles.dropdown}>
+//             <button className={styles.nav_feature_button} onClick={toggleDropdown}>
+//               Features {dropdownOpen ? <FaChevronUp  className={styles.chevron}/>
+//                : <FaChevronDown className={styles.chevron}/>}
+//             </button>
+//             {dropdownOpen && (
+//               <ul className={styles.dropdown_menu}>
+//                 <li className={styles.dropdown_item}>
+//                 <Link to="#link_shortening" onClick={() => scrollToSection('link_shortening')}>URL Shortening</Link>
+                  
+//                 </li>
+//                 <li className={styles.dropdown_item}>
+//                   <Link to="#custom_codes" onClick={() => scrollToSection('custom_codes')}>Custom URLs</Link>
+//                 </li>
+//                 <li className={styles.dropdown_item}>
+//                 <Link to="#qr_codes" onClick={() => scrollToSection('qr_codes')}>QR Codes</Link>
+//                 </li>
+//                 <li className={styles.dropdown_item}>
+//                 <Link to="#data_analytics" onClick={() => scrollToSection('data_analytics')}>Data Analytics</Link>
+//                 </li>
+//               </ul>
+//               )}
+//               </div>
+//         </li>
+
+//         <li className={styles.nav_item}>
+//           <a href='#pricing' onClick={() => scrollToSection('pricing')}>Pricing</a>
+//         </li>
+//         <li className={styles.nav_item}>
+//           <Link to="#analytics">Analytics</Link>
+//         </li>
+//         <li className={styles.nav_item}>
+//           <a href="#faqs" onClick={() => scrollToSection('faqs')}>FAQs</a>
+//         </li>
+
+//         <div className={styles.right_navbar}>
+//           <li className={styles.right_navbar_item_login}>
+//             <Link to="/login">Login</Link>
+//           </li>
+//           <li className={styles.right_navbar_item}>
+//             <button className={styles.nav_button}>Try for free</button>
+//           </li>
+//         </div>
+//       </ul>
+//     </header>
+//   );
+// };
+
+// export default Navbar;
